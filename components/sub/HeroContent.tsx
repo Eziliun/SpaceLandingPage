ae42a74c44e8b698b28951a24aabf80571550630
Eziliun <wilianw2011@hotmail.com>
2024-08-01 23:03:33 -0300

    Continuação da HeroSection

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import React from 'react'
 import {motion} from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
-import { SparklesIcon } from '@heroicons/react/24/outline'
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from "@heroicons/react/24/solid";
 
 const HeroContent = () => {
   return (
@@ -15,8 +17,13 @@ const HeroContent = () => {
             variants={slideInFromTop}
             className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
             >
-              <SparklesIcon className='text-[#b49ff] mr-[10px] h-5 w-5' />
+              <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
               <h1 className='Welcome-text text-[13px]'>Space LandingPage</h1>
+            </motion.div>
+            <motion.div
+            variants={slideInFromLeft(0.5)}
+            >
+
             </motion.div>
         </div>
     </motion.div>
